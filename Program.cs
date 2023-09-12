a4ea9ed31fe09b07fedb3f32cf18e16188657854
NinaAlejandra <niinaa___@hotmail.com>
2023-09-12 14:08:13 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the first number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the secound number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+double sum = num1 + num2;
+
+Console.WriteLine($"The sum of {num1} and {num2} is {sum}");
